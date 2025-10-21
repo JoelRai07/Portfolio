c99b16c8f46d3cecf7073975cf3afb76a3687f63
Joel <joelyerai2006@gmail.com>
2025-10-22 00:33:50 +0200

    AUR-DEV-01: First Installs

---
+++ joel-portfolio/tailwind.config.ts
@@ -0,0 +1,23 @@
+import { type Config } from "tailwindcss"
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        grid: "linear-gradient(to right, rgba(255,255,255,0.05) 1px, transparent 1px), linear-gradient(to bottom, rgba(255,255,255,0.05) 1px, transparent 1px)",
+      },
+      backgroundSize: {
+        grid: "24px 24px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
+}
+
+export default config
